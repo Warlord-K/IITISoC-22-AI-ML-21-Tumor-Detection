eaeb9d1d6fb22845b89640e7b22e9e9522c0775f
Warlord-K <95569637+Warlord-K@users.noreply.github.com>
2022-07-20 13:28:53 +0530

    added model

--- app/prediction_page.py
+++ app/prediction_page.py
@@ -1,19 +1,45 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+import tensorflow as tf
+import os
 def show_prediction_page():
     st.title("MRI Tumour Detector")
     st.write("""### Upload MRI Image""")
     uploaded_file = st.file_uploader("Choose a file",type=["jpg","png"])
     if uploaded_file is not None:
-        img = Image.open(uploaded_file)
+        img = np.array(Image.open(uploaded_file).convert("RGB").resize((224,224)))/255
         prediction = get_prediction(img)
         st.subheader(f"Tumor Type is {prediction}")
         st.image(img)
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def get_prediction(img):
-    dec = ["Meningioma","Glioma","No Tumor","Pituitary"]
-    pred = np.random.choice(dec)
-    return pred
+    dec = ["Meningioma","Pituitary","Glioma","No Tumor"]
+    model = load_model()
+    #pred = np.random.choice(dec)
+    pred = model.predict(tf.data.Dataset.from_tensor_slices([img]).batch(1))
+    return dec[np.argmax(pred)]
+    
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+    IMG_SHAPE = (224,224,3)
+    conv_layer = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                                    include_top = False,
+                                                    weights = "imagenet")
+    conv_layer.trainable = False
+
+    pooling_layer = tf.keras.layers.GlobalAveragePooling2D()
+    hidden_layer = tf.keras.layers.Dense(32,activation = "relu")
+    output_layer = tf.keras.layers.Dense(4,activation="softmax")
+    model = tf.keras.Sequential([
+        conv_layer,
+        pooling_layer,
+        hidden_layer,
+        output_layer
+    ])
+    path = os.path.realpath(__file__)[:-19]
+    model.load_weights(f'{path}\checkpoints\mobilenetv2_13epochs')
+    return model
     
