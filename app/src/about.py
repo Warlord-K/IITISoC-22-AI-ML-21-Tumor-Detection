5b2ef3a62e0b9fd1ba4ad320513d3fd741b57cdd
gauravmahendraker <mahendrakergaurav@gmail.com>
2022-07-30 22:32:03 +0530

    about updated

--- app/src/about.py
+++ app/src/about.py
@@ -44,20 +44,20 @@ def main():
             <strong><a href="https://www.linkedin.com/in/surya-karthikeya-327255228">LinkedIn</a></strong>
         </h5>
 
-        ### Yatharth Gupta
+        ### Gaurav Mahendraker
         
-        -  I'm currently learning about **Machine Learning** and **Game/App Development**
-        -  I love watching **Movies and Sit-Coms**
-        -  Learning all about **Programming and Software Development**
-        -  I'm mostly active within the **Gaming and ML/AI Community**
-        -  Ping me about **Games**, **Machine Learning**, **Game Development**, **or anything really**
+        -  I'm currently learning about **Machine Learning** and **Artifical Intelligence**
+        -  I love watching **Sports** especially **Football**
+        -  Love to learn **Programming ** and explore various uses of it
+        -  I also like doing some **Competative Programming**
+        -  Ping me about **Games**, **Machine Learning**, **or anything really**
 
         ##### Reach me
 
         <h5 align="center">
-            <strong><a href="https://github.com/Warlord-K">Github</a></strong> |
-            <strong><a href="https://www.instagram.com/_yatharthgupta/">Instagram</a></strong> |
-            <strong><a href="https://www.linkedin.com/in/yatharth-gupta-012177228/">LinkedIn</a></strong>
+            <strong><a href="https://github.com/gauravmahendraker">Github</a></strong> |
+            <strong><a href="https://www.instagram.com/gaurav_mahendraker/">Instagram</a></strong> |
+            <strong><a href="https://www.linkedin.com/in/gaurav-mahendraker-3599ab223">LinkedIn</a></strong>
         </h5>
 
         ### Yatharth Gupta
