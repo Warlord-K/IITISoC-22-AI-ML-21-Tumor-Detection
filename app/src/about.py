4321256286a2c2676af3220468fb9574b0250bee
Yashiiti <yashvashishtha03@gmail.com>
2022-08-01 16:12:52 +0530

    about

--- app/src/about.py
+++ app/src/about.py
@@ -60,20 +60,20 @@ def main():
             <strong><a href="https://www.linkedin.com/in/gaurav-mahendraker-3599ab223">LinkedIn</a></strong>
         </h5>
 
-        ### Yatharth Gupta
+        ### Yash Vashistha
         
-        -  I'm currently learning about **Machine Learning** and **Game/App Development**
-        -  I love watching **Movies and Sit-Coms**
-        -  Learning all about **Programming and Software Development**
-        -  I'm mostly active within the **Gaming and ML/AI Community**
-        -  Ping me about **Games**, **Machine Learning**, **Game Development**, **or anything really**
+        -  I'm currently learning about **Machine Learning** and **Data Analysis**
+        -  I love watching **Movies and webseries**
+        -  Learning all about **Programming** and **Automation**
+        -  I am also interested in stock markets and entrepreneurship
+        -  Ping me about **Machine Learning**, **Startups**, **or anything really**
 
         ##### Reach me
 
         <h5 align="center">
-            <strong><a href="https://github.com/surya1176">Github</a></strong> |
-            <strong><a href="https://www.instagram.com/1755_karthik/">Instagram</a></strong> |
-            <strong><a href="https://www.linkedin.com/in/surya-karthikeya-327255228">LinkedIn</a></strong>
+            <strong><a href="https://github.com/Yashiiti">Github</a></strong> |
+            <strong><a href="https://www.instagram.com/exypnos3/">Instagram</a></strong> |
+            <strong><a href="https://www.linkedin.com/in/yash-vashistha-4531461b6/">LinkedIn</a></strong>
         </h5>
         ''',
         unsafe_allow_html=True,
