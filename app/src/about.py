819f27440d4e820269aef1a5a119f84a7d6fa065
surya1176 <msuryakarthikeya@gmail.com>
2022-08-09 23:47:37 +0530

    final

--- app/src/about.py
+++ app/src/about.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
 
-def main():
-    st.markdown(
+def main(): 
+  
+    st.markdown( 
+
+
+       
+        
         '''
         
 
@@ -10,10 +15,18 @@ def main():
             About Us
         </h2>
 
-        ## The Team
+        ## The Team  
+        
 
+        ''', 
+        unsafe_allow_html=True, 
+         )  
+    col1, mid, col2 = st.columns([20,1,9])
+    with col1:
+          st.markdown( 
+        '''
         ### Yatharth Gupta
-
+         
         -  I'm currently learning about **Machine Learning** and **Game/App Development**
         -  I love watching **Movies and Sit-Coms**
         -  Learning all about **Programming and Software Development**
@@ -21,13 +34,26 @@ def main():
         -  Ping me about **Games**, **Machine Learning**, **Game Development**, **or anything really**
 
         ##### Reach me
-
+    
         <h5 align="center">
             <strong><a href="https://github.com/Warlord-K">Github</a></strong> |
             <strong><a href="https://www.instagram.com/_yatharthgupta/">Instagram</a></strong> |
             <strong><a href="https://www.linkedin.com/in/yatharth-gupta-012177228/">LinkedIn</a></strong>
-        </h5>
-
+        </h5> 
+        <br> 
+        <br>
+        ''', 
+        unsafe_allow_html=True,
+    )
+    with col2:
+         st.image("https://drive.google.com/uc?export=download&id=1N9Xly7a9wKDPYy6oHcveX-DCn9uiSzKZ", width = 260)
+    
+    
+         
+    col1, mid, col2 = st.columns([20,1,9])
+    with col1:
+          st.markdown( 
+            '''
         ### Surya Karthikeya
         
         -  I'm currently learning about **Machine Learning** and **artificial intelligence**
@@ -43,12 +69,27 @@ def main():
             <strong><a href="https://www.instagram.com/1755_karthik/">Instagram</a></strong> |
             <strong><a href="https://www.linkedin.com/in/surya-karthikeya-327255228">LinkedIn</a></strong>
         </h5>
+        <br> 
+        <br> 
+        ''', 
+        unsafe_allow_html=True,
+        )  
+ 
+    with col2:
+         st.image("https://drive.google.com/uc?export=download&id=1RJB-XmVf27ye9GBwHYDUMUI5witpyzBM", width = 260)
+    
+   
+    col1, mid, col2 = st.columns([20,1,9])
+    with col1:
+          st.markdown( 
+        '''
+    
 
         ### Gaurav Mahendraker
         
         -  I'm currently learning about **Machine Learning** and **Artifical Intelligence**
         -  I love watching **Sports** especially **Football**
-        -  Love to learn **Programming ** and explore various uses of it
+        -  Love to learn **Programming** and explore various uses of it
         -  I also like doing some **Competative Programming**
         -  Ping me about **Games**, **Machine Learning**, **or anything really**
 
@@ -58,8 +99,20 @@ def main():
             <strong><a href="https://github.com/gauravmahendraker">Github</a></strong> |
             <strong><a href="https://www.instagram.com/gaurav_mahendraker/">Instagram</a></strong> |
             <strong><a href="https://www.linkedin.com/in/gaurav-mahendraker-3599ab223">LinkedIn</a></strong>
-        </h5>
-
+        </h5> 
+        <br> 
+        <br>
+        ''', 
+        unsafe_allow_html=True, 
+        ) 
+ 
+    with col2:
+          st.image("https://drive.google.com/uc?export=download&id=1taXMLRi9TFokv9NyjOeTObBoSqieLPJe", width = 260)  
+    
+    col1, mid, col2 = st.columns([20,1,9])
+    with col1:
+         st.markdown(
+        '''
         ### Yash Vashistha
         
         -  I'm currently learning about **Machine Learning** and **Data Analysis**
@@ -74,10 +127,22 @@ def main():
             <strong><a href="https://github.com/Yashiiti">Github</a></strong> |
             <strong><a href="https://www.instagram.com/exypnos3/">Instagram</a></strong> |
             <strong><a href="https://www.linkedin.com/in/yash-vashistha-4531461b6/">LinkedIn</a></strong>
-        </h5>
+        </h5> 
+
+        <br> 
+        <br>
         ''',
-        unsafe_allow_html=True,
+        unsafe_allow_html=True, 
+    
     )
+
+ 
+    with col2:
+          st.image("https://drive.google.com/uc?export=download&id=1sErcP8FbBD0M5gMHeQV14WKY-vKzGRR8", width = 260) 
+    
+    
+    
+   
 
 
 if __name__ == '__main__':
