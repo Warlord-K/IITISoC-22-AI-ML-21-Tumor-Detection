9860c499253ffe283efb81c0576e0c804735788a
surya1176 <msuryakarthikeya@gmail.com>
2022-08-11 16:12:34 +0530

    improved error message

--- app/src/pneumonia.py
+++ app/src/pneumonia.py
@@ -30,11 +30,13 @@ def main():
         prog_bar.empty()
         st.subheader(f"Prediction is {st.session_state.prediction}")
         if st.session_state.prediction == "Bacteria Pneumonia":
-            st.markdown('##### The model indicates that the patient has Bacterial Pneumonia,Bacterial pneumonia is an infection of your lungs caused by certain bacteria. The most common one is Streptococcus (pneumococcus), but other bacteria can cause it too.<a href = "https://www.mayoclinic.org/diseases-conditions/pneumonia/symptoms-causes/syc-20354204#:~:text=and%20your%20lungs-,Pneumonia%20and%20your%20lungs,within%20your%20lungs%20(alveoli).">follow this link to know more.</a>',unsafe_allow_html=True)
+            st.error('The model indicates that the patient has Bacterial Pneumonia,Bacterial pneumonia is an infection of your lungs caused by certain bacteria. The most common one is Streptococcus (pneumococcus), but other bacteria can cause it too.')
+            st.markdown('##### <a href = "https://www.mayoclinic.org/diseases-conditions/pneumonia/symptoms-causes/syc-20354204#:~:text=and%20your%20lungs-,Pneumonia%20and%20your%20lungs,within%20your%20lungs%20(alveoli).">follow this link to know more.</a>',unsafe_allow_html=True)
         if st.session_state.prediction == "Virus Pneumonia":
-            st.markdown('##### The model indicates that the patient has Virus Pneumonia,Viral pneumonia is an infection of your lungs caused by a virus. The most common cause is the flu, but you can also get viral pneumonia from the common cold and other viruses.<a href = "https://www.mayoclinic.org/diseases-conditions/pneumonia/symptoms-causes/syc-20354204#:~:text=and%20your%20lungs-,Pneumonia%20and%20your%20lungs,within%20your%20lungs%20(alveoli).">follow this link to know more.</a>',unsafe_allow_html=True)        
+            st.error('The model indicates that the patient has Virus Pneumonia,Viral pneumonia is an infection of your lungs caused by a virus. The most common cause is the flu, but you can also get viral pneumonia from the common cold and other viruses.')
+            st.markdown('##### <a href = "https://www.mayoclinic.org/diseases-conditions/pneumonia/symptoms-causes/syc-20354204#:~:text=and%20your%20lungs-,Pneumonia%20and%20your%20lungs,within%20your%20lungs%20(alveoli).">follow this link to know more.</a>',unsafe_allow_html=True)        
         if st.session_state.prediction == "Normal":
-            st.markdown('##### Congratulations,the model indicates that the patient is normal,if you seem to get any symptoms please consult a doctor.')
+            st.success('Congratulations,the model indicates that the patient is normal,if you seem to get any symptoms please consult a doctor.')
         st.image(st.session_state.img)
 
         
